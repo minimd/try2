d7fcc39edf1e4a54439e3ef8de7a7cca17151b73
mohammed bahaa <139212704+minimd@users.noreply.github.com>
2024-04-29 20:03:46 +0300

    questions added

---
+++ flutter_application_2/lib/models/quests.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_application_2/models/data.dart';
+
+const questions = [
+  QuizQuestion(
+    'what are the main building blocks of flutter UIs?',
+    ["  widgets", 'components, ' 'blocks', "functions"],
+  ),
+  QuizQuestion(
+    "What is the capital of France?",
+    [
+      'Paris',
+      'Madrid,'
+          'Rome',
+      'Berlin,'
+    ],
+  ),
+  QuizQuestion(
+    'Which planet is known as the Red Planet?',
+    ['Mars', 'Jupiter', 'Saturn', "Venus"],
+  ),
+  QuizQuestion(
+      'What is the chemical symbol for water?', ['H2O', 'CO2', 'NaCl', 'O2']),
+  QuizQuestion('What is the largest continent on Earth?',
+      ['Asia', 'Africa', 'Europe', 'Australia']),
+  QuizQuestion('Which animal is known as the King of the Jungle?',
+      ['Lion', 'Elephant', 'Tiger', 'Bear']),
+];
